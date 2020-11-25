b97a9d59c9885134c26d665082c87a2f14431ee3
12138-java <60643142+12138-java@users.noreply.github.com>
2020-11-25 21:14:01 +0800

    Update RuoYiApplication.java

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -16,8 +16,8 @@ public class RuoYiApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依kinwgan启动成功   ლ(´ڡ`ლ)ﾞ  \n");
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功111111111   ლ(´ڡ`ლ)ﾞ  \n" +
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依kin2sasdasd2222ew raan2启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依2222启222动2成功12222221111   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
                 " |  _ _   \\      \\   \\   /  /    \n" +
                 " | ( ' )  |   asdasd    \\  _. /  '       \n" +
