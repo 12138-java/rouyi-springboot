6e2f85eee26dd29814fa32d4f3a865b44efceb46
13338585229 <1292769254@qq.com>
2020-11-25 21:08:13 +0800

    冲突测试

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -16,11 +16,8 @@ public class RuoYiApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-<<<<<<< HEAD
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依kinwgan启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-=======
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依kinwgan启动成功   ლ(´ڡ`ლ)ﾞ  \n");
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功111111111   ლ(´ڡ`ლ)ﾞ  \n" +
->>>>>>> 1695d6bbd88060eb28609a2818ad19c66c0af0d5
                 " .-------.       ____     __        \n" +
                 " |  _ _   \\      \\   \\   /  /    \n" +
                 " | ( ' )  |   asdasd    \\  _. /  '       \n" +
